dd97be10d5ab70610a19ccc1d115e3b10eebda53
sayandas-sd <sayandasmac@gmail.com>
2024-12-17 12:53:08 +0530

    added initial gin route

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+
+	routes.authRouter(r)
+	routes.userRouter(r)
+
+	r.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "access granted for api-1",
+		})
+	})
+
+	r.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "access granted for api-2",
+		})
+	})
+
+	log.Printf("server is running on port %s...", port)
+
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+
+}
